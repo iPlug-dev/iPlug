b8697c055b3e3d80815b3cbdcb41743217107c24
CookieMichal <MrCookieMichal@gmail.com>
2014-01-04 03:33:27 +0100

    Update iPlug.js

--- Chrome/javascripts/iPlug.js
+++ Chrome/javascripts/iPlug.js
@@ -1,14 +1,17 @@
 function init() {
     if ($('#audience').length > 0) {
-        if (document.location.pathname == "/") return; //Only one instance of plug at a time.
-        console.log("[iPlug]: Loading script...");
+        if (document.location.pathname == "/") {
+            console.log("[iPlug]: Script will not be loaded!")
+        } else {
+                   console.log("[iPlug]: Loading script...");
         $.getScript(chrome.extension.getURL("javascripts/iPlugApp.js"))
             .done(function (script, status, statusid) {
             console.log("[iPlug]: Script loaded!");
         })
             .fail(function () {
             console.warn("[iPlug]: Script failed to load!");
-        });
+        }); 
+        }
     } else {
         setTimeout(init, 250);
     }
