e155e85c9f3c58292819895e9ee5c0c985b282a5
CookieMichal <MrCookieMichal@gmail.com>
2014-01-04 04:13:56 +0100

    Update iPlug.js

--- Chrome/javascripts/iPlug.js
+++ Chrome/javascripts/iPlug.js
@@ -1,16 +1,16 @@
 function init() {
     if ($('#audience').length > 0) {
         if (document.location.pathname == "/") {
-            console.log("[iPlug]: Script will not be loaded!")
+            console.log("[iPlug]: Script will not be loaded!");
         } else {
-                   console.log("[iPlug]: Loading script...");
-        $.getScript(chrome.extension.getURL("javascripts/iPlugApp.js"))
-            .done(function (script, status, statusid) {
-            console.log("[iPlug]: Script loaded!");
-        })
-            .fail(function () {
-            console.warn("[iPlug]: Script failed to load!");
-        }); 
+            console.log("[iPlug]: Loading script...");
+            $.getScript(chrome.extension.getURL("javascripts/iPlugApp.js"))
+                .done(function (script, status, statusid) {
+                console.log("[iPlug]: Script loaded!");
+            })
+                .fail(function () {
+                console.warn("[iPlug]: Script failed to load!");
+            });
         }
     } else {
         setTimeout(init, 250);
