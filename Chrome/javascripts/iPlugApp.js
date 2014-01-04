5b2a7e5258b7f6a8cac91335d1ba0ef0b40c140c
CookieMichal <MrCookieMichal@gmail.com>
2014-01-04 15:25:32 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,2 +1,1 @@
-$("#chat-header").append("<div style='display: none' id='private-refresh-icon'>Refresh user list<i class='icon icon-refresh-video'></i></div>");
-$("#chat-header").append("<div style='display: none' id='private-go-back-icon'><i class='icon icon-waitlist-full'></i></div>");
+console.log('hello')
