e2e498b4c753021eec4440c56b2f6de75b4ee751
CookieMichal <MrCookieMichal@gmail.com>
2014-01-05 18:54:32 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,1 +1,3 @@
 javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-javascript'); jsCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/javascript.js'); document.body.appendChild(jsCode); }());
+
+$("#plug-dj").after("<div id='iplug-menu-icon'><i class='iplug-logo'></i></div>");
