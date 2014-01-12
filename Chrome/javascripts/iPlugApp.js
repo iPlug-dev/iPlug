57b8449071da53d9e3ad1e2ba504dd36192de849
CookieMichal <MrCookieMichal@gmail.com>
2014-01-12 15:38:57 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,9 +1,12 @@
-javascript: (function (){var jssCode = document.createElement('script'); jssCode.setAttribute('id', 'iPlug-javascript'); jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js'); document.body.appendChild(jssCode); }());
-
-
-for (var i = 0;i < iPlugscripts.length; i++) {
-  function lol () {
-    javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-' + iPlugscripts[i].substring(0,iPlugscripts.length - 3)); jsCode.setAttribute('src', iPlugscripts[i]); document.body.appendChild(jsCode); }());
-  }
-  lol()
-}
+(function () {
+    var jssCode = document.createElement('script');
+    jssCode.setAttribute('id', 'iPlug-javascript');
+    jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js');
+    document.body.appendChild(jssCode);
+    for (var i = 0; i < iPlugscripts.length; i++) {
+        var jsCode = document.createElement('script');
+        jsCode.setAttribute('id', 'iPlug-' + iPlugscripts[i].substring(0, iPlugscripts.length - 3));
+        jsCode.setAttribute('src', iPlugscripts[i]);
+        document.body.appendChild(jsCode);
+    }
+}());
