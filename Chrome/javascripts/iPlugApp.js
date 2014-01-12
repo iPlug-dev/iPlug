ab4c6bfbfe4087f44d8ed8a693cba823d30c82fd
CookieMichal <MrCookieMichal@gmail.com>
2014-01-12 15:03:34 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,14 +1,1 @@
 javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-javascript'); jsCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/javascript.js'); document.body.appendChild(jsCode); }());
-
-$("#plug-dj").after("<div id='iplug-menu-icon'><i id='iplug-menu-plug'></i><i class='iplug-logo'></i></div>");
-iPlug = {};
-iPlug.plug3check = function () {
-    if (typeof plugCubed == "undefined") {
-        setTimeout(iPlug.plug3check, 10000);
-        return;
-    }
-    setTimeout(function(){
-        $("#room-bar").css('left', '162px');
-    },8000);
-};
-setTimeout(iPlug.plug3check, 10000);
