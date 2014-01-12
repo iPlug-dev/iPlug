1b91055c7bb2416583aae2e04ec757c222affbea
CookieMichal <MrCookieMichal@gmail.com>
2014-01-12 15:52:04 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -8,7 +8,7 @@ listscripts = [
 for (var i = 0; i < listscripts.length; i++) {
 var jsCode = document.createElement('script');
 jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(0, iPlugscripts.length - 3));
-jsCode.setAttribute('src', iPlugscripts[i]);
+jsCode.setAttribute('src', listscripts[i]);
 document.body.appendChild(jsCode);
 }
 
