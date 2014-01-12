cc41185cf69bb5f452f07d4e5ff56471fa55dd89
CookieMichal <MrCookieMichal@gmail.com>
2014-01-12 15:03:59 +0100

    Update javascript.js

--- Source/javascript.js
+++ Source/javascript.js
@@ -1,1 +1,13 @@
-console.log("I love cookies <3");
+//Put all code here
+$("#plug-dj").after("<div id='iplug-menu-icon'><i id='iplug-menu-plug'></i><i class='iplug-logo'></i></div>");
+iPlug = {};
+iPlug.plug3check = function () {
+    if (typeof plugCubed == "undefined") {
+        setTimeout(iPlug.plug3check, 10000);
+        return;
+    }
+    setTimeout(function(){
+        $("#room-bar").css('left', '162px');
+    },8000);
+};
+setTimeout(iPlug.plug3check, 10000);
