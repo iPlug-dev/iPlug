ce119165c65d7bbb5aea2dc1dbe6103955386d0f
L0laapk3 <L0laapk3@gmail.com>
2014-01-12 15:26:53 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,1 +1,9 @@
-javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-javascript'); jsCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/javascript.js'); document.body.appendChild(jsCode); }());
+function () {
+javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-javascript'); jsCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js'); document.body.appendChild(jsCode); }());
+}
+
+for (var i = 0;i < iPlugscripts.length; i++) {
+  function () {
+    javascript: (function (){var jsCode = document.createElement('script'); jsCode.setAttribute('id', 'iPlug-' + iPlugscripts[i].substring(0,iPlugscripts.length - 3)); jsCode.setAttribute('src', iPlugscripts[i]); document.body.appendChild(jsCode); }());
+  }
+}
