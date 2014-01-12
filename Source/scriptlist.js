9ebce3c8e3808a10c0c8d55015bf536b53ca6ece
L0laapk3 <L0laapk3@gmail.com>
2014-01-12 15:21:01 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -1,1 +1,4 @@
-iPlugscripts = {https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js,https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js}
+iPlugscripts = [
+  https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js
+ ,https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js
+]
