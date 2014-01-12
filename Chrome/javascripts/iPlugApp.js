13179dcdcaac36f01fcf024b3a0ebd0a82c61d8b
L0laapk3 <L0laapk3@gmail.com>
2014-01-12 15:47:43 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -2,6 +2,6 @@ $.getScript("https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js")
 iPlugscripts = listscripts()
     for (var i = 0; i < iPlugscripts.length; i++) {
         $.getScript(iPlugscripts[i]);
-        function lol();
+        lol();
     }
 
