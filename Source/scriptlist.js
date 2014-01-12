0325628b4042e7dc8dae9b7150f59740ea2aa76f
CookieMichal <MrCookieMichal@gmail.com>
2014-01-12 15:55:55 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -1,15 +1,12 @@
-listscripts = [
-  "https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js",
-  "https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js"
-];
-
-
-
-for (var i = 0; i < listscripts.length; i++) {
-var jsCode = document.createElement('script');
-jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(0, listscripts.length - 3));
-jsCode.setAttribute('src', listscripts[i]);
-document.body.appendChild(jsCode);
-}
-
-
+(function () {
+    listscripts = [
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js",
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js"];
+    
+    for (i = 0; i < listscripts.length; i++) {
+        var jsCode = document.createElement('script');
+        jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(0, listscripts.length - 3));
+        jsCode.setAttribute('src', listscripts[i]);
+        document.body.appendChild(jsCode);
+    }
+}());
