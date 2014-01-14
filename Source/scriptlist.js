1490f77cff8abe06eb2330de46dceaf14c3a6135
CookieMichal <MrCookieMichal@gmail.com>
2014-01-14 20:35:52 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -2,17 +2,20 @@
     listscripts = [
         "https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js",
         "https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js"];
-        
-    console.log("[iPlug]: Loading more scripts...")
+
+    console.log("[iPlug]: Loading more scripts...");
 
     for (i = 0; i <= listscripts.length; i++) {
-        if (i = listscripts.length) {
+        if (i < listscripts.length) {
+            var jsCode = document.createElement('script');
+            jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(52, listscripts.length - 3));
+            jsCode.setAttribute('src', listscripts[i]);
+            document.body.appendChild(jsCode);
+        } else if (i == listscripts.length) {
             console.log("[iPlug]: Scripts loaded!");
             return;
+        } else {
+            console.log("[iPlug]: Error!");
         }
-        var jsCode = document.createElement('script');
-        jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(52, listscripts.length - 3));
-        jsCode.setAttribute('src', listscripts[i]);
-        document.body.appendChild(jsCode);
     }
 }());
