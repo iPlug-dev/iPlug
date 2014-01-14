e570615259755c95393f5f79573721c7cda8d36e
CookieMichal <MrCookieMichal@gmail.com>
2014-01-14 20:28:06 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -2,10 +2,16 @@
     listscripts = [
         "https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js",
         "https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js"];
-    
-    for (i = 0; i < listscripts.length; i++) {
+        
+    console.log("[iPlug]: Loading more scripts...")
+
+    for (i = 0; i <= listscripts.length; i++) {
+        if (i = listscripts.length) {
+            console.log("[iPlug]: Scripts loaded!");
+            return;
+        }
         var jsCode = document.createElement('script');
-        jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(0, listscripts.length - 3));
+        jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(52, listscripts.length - 3));
         jsCode.setAttribute('src', listscripts[i]);
         document.body.appendChild(jsCode);
     }
