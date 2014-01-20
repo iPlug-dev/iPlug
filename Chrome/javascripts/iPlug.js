07030182f2cbfceedf0a9c30702f93d0dd727a82
CookieMichal <MrCookieMichal@gmail.com>
2014-01-20 23:14:26 +0100

    Update iPlug.js

--- Chrome/javascripts/iPlug.js
+++ Chrome/javascripts/iPlug.js
@@ -1,48 +1,56 @@
 function init() {
     if ($('#audience').length > 0) {
-        LoadScripts();
+        if (document.location.pathname == "/") {
+            console.log("[iPlug]: Script will not be loaded here!");
+        } else {
+            var scripts = ["javascripts/jquery-ui-1.10.3.custom.js",
+            //"javascripts/socket.io.js", | plug already got it :o
+            "javascripts/ddslick.js",
+                "javascripts/bililiteRange.js",
+                "javascripts/jquery.simulate.js",
+                "javascripts/jquery.simulate.ext.js",
+                "javascripts/jquery.simulate.drag-n-drop.js",
+                "javascripts/jquery.simulate.key-sequence.js",
+                "javascripts/jquery.simulate.key-combo.js"];
+            console.log("[iPlug]: Loading components...");
+            loadItall(scripts, 0);
+        }
     } else {
         setTimeout(init, 500);
     }
 }
-init();
 
-function LoadScripts() {
-    var scripts = [];
-    var _scripts = ["javascripts/jquery-ui-1.10.3.custom.js",
-    //"javascripts/socket.io.js", | plug already got it :o
-    "javascripts/ddslick.js",
-        "javascripts/bililiteRange.js",
-        "javascripts/jquery.simulate.js",
-        "javascripts/jquery.simulate.ext.js",
-        "javascripts/jquery.simulate.drag-n-drop.js",
-        "javascripts/jquery.simulate.key-sequence.js",
-        "javascripts/jquery.simulate.key-combo.js"];
-    LoadScriptsAsync(_scripts, scripts);
-}
-
-function LoadScriptsAsync(_scripts, scripts) {
-    for (var i = 0; i < _scripts.length; i++) {
-        loadScript(i, _scripts[i], scripts[i], _scripts.length);
-    }
-}
-
-function loadScript(awesomenumber, src, script, x) {
-    script = document.createElement('script');
-    script.onerror = function () {
-        callback(false, awesomenumber, x);
-    };
-    script.onload = function () {
-        callback(true, awesomenumber, x);
-    };
-    script.src = chrome.extension.getURL(src);
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-function callback(check, number, x) {
+function callback(check, num, x) {
     if (check === true) {
-        console.log("[iPlug]: Loading component " + (1+number) + "/" + x + " finished with succes.");
+        console.log("[iPlug]: Loading component " + (1 + num) + "/" + x + " finished with succes.");
     } else if (check === false) {
-        console.warn("[iPlug]: Loading component " + (1+number) + "/" + x + " finished with error.");
+        console.warn("[iPlug]: Loading component " + (1 + num) + "/" + x + " finished with error.");
+    } else {
+        console.warn("[iPlug]: This should not happen. Report it.");
     }
 }
+
+function loadItall(scripts, num) {
+    if (num > scripts.length) {
+        return;
+    } else if (num == scripts.length) {
+        console.log("[iPlug]: Loading main script...");
+        $.getScript(chrome.extension.getURL("javascripts/iPlugApp.js"))
+            .done(function () {
+            console.log("[iPlug]: Main script loaded!");
+        })
+            .fail(function () {
+            console.warn("[iPlug]: Main script failed to load!");
+        });
+    } else if (num < scripts.length) {
+        $.getScript(chrome.extension.getURL(scripts[num]))
+            .done(function () {
+            callback(true, num, scripts.length);
+            loadItall((num + 1));
+        })
+            .fail(function () {
+            callback(false, num, scripts.length);
+        });
+    }
+}
+init();
