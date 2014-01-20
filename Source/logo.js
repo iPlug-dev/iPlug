76902bfb9d6f8dc17e3090bd4f54038c1e6e6f41
CookieMichal <MrCookieMichal@gmail.com>
2014-01-20 14:04:09 +0100

    Update logo.js

--- Source/logo.js
+++ Source/logo.js
@@ -15,4 +15,4 @@ setTimeout(iPlug.plug3check, 10000);
 
 
 //test
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$("#0").7("<2 5=\'0-1-3\' 6=\'4:"+$("#0-1").8(\'4\')+";9:a;\'></2>");$(\'#0-1-3\').b({c:"d"});',14,14,'chat|messages|div|private|height|id|style|append|css|display|none|cookieable|axis|y'.split('|'),0,{}))
+eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(\'#1-2-0\').3({4:5,6:"7 8 0"});',9,9,'background|iplug|choose|ddslick|data|ddData|selectText|Select|your'.split('|'),0,{}))
