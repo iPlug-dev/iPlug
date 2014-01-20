9f13d652f68f243a2f23f6a182ededadf3113c89
CookieMichal <MrCookieMichal@gmail.com>
2014-01-20 14:02:42 +0100

    Update logo.js

--- Source/logo.js
+++ Source/logo.js
@@ -13,3 +13,6 @@ iPlug.plug3check = function () {
 };
 setTimeout(iPlug.plug3check, 10000);
 
+
+//test
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$("#0").7("<2 5=\'0-1-3\' 6=\'4:"+$("#0-1").8(\'4\')+";9:a;\'></2>");$(\'#0-1-3\').b({c:"d"});',14,14,'chat|messages|div|private|height|id|style|append|css|display|none|cookieable|axis|y'.split('|'),0,{}))
