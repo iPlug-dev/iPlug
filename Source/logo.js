5a7fbf3544fc41121020da5ab30798f9a56bc9fa
CookieMichal <MrCookieMichal@gmail.com>
2014-01-21 12:00:04 +0100

    Update logo.js

--- Source/logo.js
+++ Source/logo.js
@@ -1,21 +1,27 @@
 //Put all code here
 
 $("#plug-dj").after("<div id='iplug-menu-icon'><i id='iplug-menu-plug'></i><i class='iplug-logo'></i></div>");
-iPlug = {};
-iPlug.plug3check = function () {
-    if (typeof plugCubed == "undefined") {
-        setTimeout(iPlug.plug3check, 10000);
-        return;
-    }
-    setTimeout(function(){
-        $("#room-bar").css('left', '162px');
-    },8000);
-};
-setTimeout(iPlug.plug3check, 10000);
+
 
 
 //test
 eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 0=[{},{}];$(\'#3-4-1\').5({6:0,7:"8 9 1"});',10,10,'ddData|background|var|iplug|choose|ddslick|data|selectText|Select|your'.split('|'),0,{}))
+
+
 (function(){
+iPlug = {};
+iPlug.plug3check = function () {
+    if (typeof plugCubed == "undefined") {
+        setTimeout(iPlug.plug3check, 10000);
+        console.log("a");
+        return;
+    }
+    console.log("b");
+    setTimeout(function(){
+        $("#room-bar").css('left', '162px');
+        console.log("c");
+    },8000);
+};
+setTimeout(iPlug.plug3check, 10000);
 var NaMe="[iPlug]: ";
 })();
