35b8a4fb5888d89f9d79b42f48547a3fee34aaeb
CookieMichal <MrCookieMichal@gmail.com>
2014-01-21 11:54:24 +0100

    Update logo.js

--- Source/logo.js
+++ Source/logo.js
@@ -16,5 +16,6 @@ setTimeout(iPlug.plug3check, 10000);
 
 //test
 eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 0=[{},{}];$(\'#3-4-1\').5({6:0,7:"8 9 1"});',10,10,'ddData|background|var|iplug|choose|ddslick|data|selectText|Select|your'.split('|'),0,{}))
-
+(function(){
 var NaMe="[iPlug]: ";
+}())
