4c22183de3d02d90e41f6d243eb53e99d1eaac67
CookieMichal <MrCookieMichal@gmail.com>
2014-01-21 13:04:43 +0100

    Update logo.js

--- Source/logo.js
+++ Source/logo.js
@@ -1,26 +1,42 @@
-//Put all code here
-
-$("#plug-dj").after("<div id='iplug-menu-icon'><i id='iplug-menu-plug'></i><i class='iplug-logo'></i></div>");
-
-
-
-//test
-eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 0=[{},{}];$(\'#3-4-1\').5({6:0,7:"8 9 1"});',10,10,'ddData|background|var|iplug|choose|ddslick|data|selectText|Select|your'.split('|'),0,{}))
-
-
 code = (function() {
+    
     var iPlug = {};
     iPlug.plug3check = function () {
         if (typeof plugCubed == "undefined") {
             setTimeout(iPlug.plug3check, 10000);
-            console.log("a");
             return;
         }
-        console.log("b");
+        console.log("[iPlug]: Plug³ detected!");
+        console.log("[iPlug]: Moving UI");
         setTimeout(function () {
             $("#room-bar").css('left', '162px');
-            console.log("c");
+            console.log("[iPlug]: UI moved!");
         }, 8000);
     };
     setTimeout(iPlug.plug3check, 10000);
+    $("#plug-dj").after("<div id='iplug-menu-icon'><i id='iplug-menu-plug'></i><i class='iplug-logo'></i></div>");
+
+    //put all code here
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }());
