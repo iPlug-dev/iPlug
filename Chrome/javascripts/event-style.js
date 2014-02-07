694d985104a04da1794afedad0649e62aa505090
CookieMichal <MrCookieMichal@gmail.com>
2014-02-07 21:09:06 +0100

    Update and rename event-style.js to attrchange.js

--- Chrome/javascripts/event-style.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var ev = new $.Event('style'),
-        orig = $.fn.css;
-    $.fn.css = function() {
-        orig.apply(this, arguments);
-        $(this).trigger(ev);
-    }
-})();
