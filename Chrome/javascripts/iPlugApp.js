b1be5d284fbc1348a4bcb974262367ec06cf274a
CookieMichal <MrCookieMichal@gmail.com>
2014-02-15 18:31:34 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,15 +1,16 @@
-var jssCode = document.createElement('script');
-jssCode.setAttribute('id', 'iPlug-javascript');
-jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js');
-document.body.appendChild(jssCode);
-
-var version = "0.6.0";
-
-
-if (localStorage['iPlug|version'] != version) {
-    localStorage['iPlug|version'] = version;
-    localStorage['iPlug|updated'] = true;
-} else {
-    localStorage['iPlug|updated'] = false;
-}
-
+(function () {
+    var jssCode = document.createElement('script');
+    jssCode.setAttribute('id', 'iPlug-javascript');
+    jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js');
+    document.body.appendChild(jssCode);
+    
+    var version = "0.6.0";
+    
+    
+    if (localStorage['iPlug|version'] != version) {
+        localStorage['iPlug|version'] = version;
+        localStorage['iPlug|updated'] = true;
+    } else {
+        localStorage['iPlug|updated'] = false;
+    }
+}());
