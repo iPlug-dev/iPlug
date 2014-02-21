b0a24694cbb97755a7040cde93670e52d9a86e14
CookieMichal <MrCookieMichal@gmail.com>
2014-02-21 15:05:17 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -1,21 +1,21 @@
 (function () {
     listscripts = [
-        "https://raw.github.com/L0laapk3/iPlug/master/Source/logo.js",
-        "https://raw.github.com/L0laapk3/iPlug/master/Source/chatterbox.js"];
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "logo.js",
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "chatterbox.js"];
 
-    console.log("[iPlug]: Loading more scripts...");
+    
 
     for (i = 0; i <= listscripts.length; i++) {
         if (i < listscripts.length) {
-            var jsCode = document.createElement('script');
-            jsCode.setAttribute('id', 'iPlug-' + listscripts[i].substring(52, listscripts[i].length - 3));
-            jsCode.setAttribute('src', listscripts[i]);
-            document.body.appendChild(jsCode);
+            $.getScript(listscripts[i])
+                .fail(function() {
+                    console.warn("[iPlug]: Script number " + i + " wasn't loaded!")
+                });
         } else if (i == listscripts.length) {
             console.log("[iPlug]: Scripts loaded!");
             return;
         } else {
-            console.log("[iPlug]: Error!");
+            console.warn("[iPlug]: Error!");
         }
     }
 }());
