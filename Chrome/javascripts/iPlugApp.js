7f504ef682af90251ebb0eb9917a6bb144a75a92
CookieMichal <MrCookieMichal@gmail.com>
2014-02-21 15:39:53 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,13 +1,4 @@
 (function () {
-    
-    console.log("[iPlug]: Loading more scripts...");
-    
-    $.getScript("https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js")
-            .fail(function () {
-            console.error("[iPlug]: Loading more scripts finished with error!");
-        });
-        
-        
     var version = "0.6.0";
     
     
@@ -17,4 +8,14 @@
     } else {
         localStorage['iPlug|updated'] = false;
     }
+    
+    console.log("[iPlug]: Loading more scripts...");
+    
+    $.getScript("https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js")
+            .fail(function () {
+            console.error("[iPlug]: Loading more scripts finished with error!");
+        });
+        
+        
+
 }());
