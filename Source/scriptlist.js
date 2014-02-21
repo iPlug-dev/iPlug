04e119545372b817473bdf8ae14a99fabd332e89
CookieMichal <MrCookieMichal@gmail.com>
2014-02-21 15:13:26 +0100

    Update scriptlist.js

--- Source/scriptlist.js
+++ Source/scriptlist.js
@@ -1,7 +1,7 @@
 (function () {
     listscripts = [
-        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "logo.js",
-        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "chatterbox.js"];
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "/logo.js",
+        "https://raw.github.com/L0laapk3/iPlug/master/Source/" + version + "/chatterbox.js"];
 
     
 
@@ -9,7 +9,7 @@
         if (i < listscripts.length) {
             $.getScript(listscripts[i])
                 .fail(function() {
-                    console.warn("[iPlug]: Script number " + i + " wasn't loaded!")
+                    console.warn("[iPlug]: Script number " + (i+1) + " wasn't loaded!")
                 });
         } else if (i == listscripts.length) {
             console.log("[iPlug]: Scripts loaded!");
