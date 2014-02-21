5524367691293845995b8898ff223ae950065390
CookieMichal <MrCookieMichal@gmail.com>
2014-02-21 14:38:45 +0100

    Update iPlugApp.js

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,9 +1,19 @@
 (function () {
     var jssCode = document.createElement('script');
     jssCode.setAttribute('id', 'iPlug-javascript');
-    jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js');
+    jssCode.setAttribute('src', 'https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlistx.js'); // x is here for tests
     document.body.appendChild(jssCode);
     
+    $.getScript("https://raw.github.com/L0laapk3/iPlug/master/Source/scriptlist.js")
+            .done(function () {
+            console.log("[iPlug]: Loading more scripts...");
+        })
+            .fail(function (x) {
+            console.warn("[iPlug]: Loading more scripts finished with error!");
+            console.warn(x);
+        });
+        
+        
     var version = "0.6.0";
     
     
