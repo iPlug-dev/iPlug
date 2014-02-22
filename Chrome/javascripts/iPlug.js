ddccf611a8b4c3f91ac5130f24efeec4b96760ca
CookieMichal <MrCookieMichal@gmail.com>
2014-02-22 13:16:16 +0100

    Update iPlug.js

--- Chrome/javascripts/iPlug.js
+++ Chrome/javascripts/iPlug.js
@@ -5,8 +5,8 @@ function init() {
         } else {
                 document.addEventListener('getURL', function(event) {
                     console.log(event);
-                    var x = chrome.extension.getURL(event.url);
-                    var fetchResponse = new CustomEvent('readyURL-'+event.reqID, {"url": x, "reqID": event.reqID});
+                    var x = chrome.extension.getURL(event.detail.url);
+                    var fetchResponse = new CustomEvent('readyURL-'+event.detail.reqID, {"url": x, "reqID": event.detail.reqID});
                     document.dispatchEvent(fetchResponse);
                 });
                 
