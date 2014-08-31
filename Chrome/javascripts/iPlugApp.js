c794ba634dde828c2ba4eee39251ed555619f595
CookieMichal <MrCookieMichal@Gmail.com>
2014-08-31 17:46:14 +0200

    Another quick fix
    Signed-off-by: CookieMichal <MrCookieMichal@Gmail.com>

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -5,6 +5,12 @@
     API.on(API.ADVANCE, function(){
         setTimeout(function(){
             $("#woot").click();
+        },5000);
+    });
+
+    API.on(API.WAIT_LIST_UPDATE, function(){
+        $(".is-wait").click();
+        setTimeout(function(){
             $(".is-wait").click();
         },5000);
     });
