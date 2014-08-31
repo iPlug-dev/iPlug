b89899762ab6c3643a807eed47909b108267b3e3
CookieMichal <MrCookieMichal@Gmail.com>
2014-08-31 15:17:38 +0000

    iPlug.js edited online with Bitbucket

--- Chrome/javascripts/iPlug.js
+++ Chrome/javascripts/iPlug.js
@@ -3,24 +3,12 @@ function init() {
         if (document.location.pathname == "/") {
             console.log("[iPlug]: Script will not be loaded here!");
         } else {
-                document.addEventListener('getURL', function(event) {
-                    var x = chrome.extension.getURL(event.detail.url);
-                    var fetchResponse = new CustomEvent('readyURL-'+event.detail.reqID, {"detail": {"url": x, "reqID": event.detail.reqID}});
-                    document.dispatchEvent(fetchResponse);
-                });
-                
-                
-            var scripts = ["javascripts/jquery-ui-1.10.3.custom.js",
-                "javascripts/attrchange.js",
-                "javascripts/chosen.jquery.min.js",
-                "javascripts/bililiteRange.js",
-                "javascripts/jquery.simulate.js",
-                "javascripts/jquery.simulate.ext.js",
-                "javascripts/jquery.simulate.drag-n-drop.js",
-                "javascripts/jquery.simulate.key-sequence.js",
-                "javascripts/jquery.simulate.key-combo.js",
-                "javascripts/ydn.db-isw-core-qry.js",
-                "javascripts/socket.io.js"];
+            document.addEventListener('getURL', function(event) {
+                var x = chrome.extension.getURL(event.detail.url);
+                var fetchResponse = new CustomEvent('readyURL-'+event.detail.reqID, {"detail": {"url": x, "reqID": event.detail.reqID}});
+                document.dispatchEvent(fetchResponse);
+            });    
+            var scripts = [];
             console.log("[iPlug]: Loading components...");
             loadItall(scripts, 0);
         }
