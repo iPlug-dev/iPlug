0f58a73d93f8abf8414191129119c41c75468f19
CookieMichal <MrCookieMichal@Gmail.com>
2014-09-02 16:32:54 +0000

    added info bout update

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -1,5 +1,6 @@
 (function () {
     "use strict";
+    if (typeof(window.iPlugDebug) != "boolean") window.iPlugDebug = false;
     API.on(API.ADVANCE, function(){
         setTimeout(function(){
             $("#woot").click();
@@ -11,6 +12,7 @@
             $(".is-wait").click();
         },5000);
     });
+    $("body").append("<div id='iplug-overlay' style='display:none;'></div>");
     $("#waitlist > .header > .divider").remove();
     $("#waitlist > .header").append("<div class='divider left'></div>");
     $("#waitlist > .header").append("<div class='divider right'></div>");
@@ -48,13 +50,36 @@
         $(".user > .icon-grab").attr("style", "margin-right: 30px;");
         $(".user > .name").attr("style", "left: 68px");
         $(".user > .icon-woot, .leveldisplay").remove();
-        console.log(users, $("#list-filter-input").val());
+        if (window.iPlugDebug){console.log(users, $("#list-filter-input").val());}
         users = users.filter(function(user) {return -1 != user.username.toLowerCase().indexOf($("#list-filter-input").val().toLowerCase());});
-        console.log(users);
+        if (window.iPlugDebug){console.log(users);}
         for (i = 0; i < users.length; i++) {
             $($(".user > .name")[i]).parent().append(["<i class='icon icon-woot' style='background-position: -174px -280px'></i>", "", "<i class='icon icon-woot'></i>"][users[i].vote + 1] + "<div class='leveldisplay' style='left:30px; height: 30px; width: 46px'><span class='name' style='top: 7px; margin-left: auto; margin-right: auto; color: #eee; font-size: 10px'>lvl" + users[i].level + "</span></div>");
         }
     }
     
+    function version() {
+        var v;
+        var reqID = Math.random().toString();
+        var fetchResponse = new CustomEvent('checkVersion', {
+            "detail": {
+                "reqID": reqID
+            }
+        });
+        document.addEventListener('checkedVersion-' + reqID, function respListener(e) {
+            if (e.detail.reqID == reqID) {
+                document.removeEventListener('checkedVersion-' + reqID, respListener);
+                v = e.detail.updated;
+            }
+        });
+        document.dispatchEvent(fetchResponse);
+        return v;
+
+    };
+    
+    if(version){
+        $('#iplug-overlay').css('display', 'block');
+        alert("iPlug has been updated!\n\nNext update: fix this alert!\nNext update: working menu!");
+    }
     
 }());
