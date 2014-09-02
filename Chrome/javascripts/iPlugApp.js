78c9ca1fefd79bb976cde2c5e016c5acaed6e11e
CookieMichal <MrCookieMichal@Gmail.com>
2014-09-02 16:27:20 +0200

    Fixed small visual bug
    Signed-off-by: CookieMichal <MrCookieMichal@Gmail.com>

--- Chrome/javascripts/iPlugApp.js
+++ Chrome/javascripts/iPlugApp.js
@@ -11,8 +11,11 @@
             $(".is-wait").click();
         },5000);
     });
-    
-    $("#header-panel-bar").append("<div id='iplug-button' class='header-panel-button'>\
+    $("#waitlist > .header > .divider").remove();
+    $("#waitlist > .header").append("<div class='divider left'></div>");
+    $("#waitlist > .header").append("<div class='divider right'></div>");
+    $("#header-panel-bar").append("\
+    <div id='iplug-button' class='header-panel-button'>\
         <div class='box'>\
             <i class='icon-iplug'></i>\
             <span>iPlug</span>\
@@ -46,12 +49,10 @@
         }, 0);
         $("#wootchangetracker").unbind().remove();
         var users = API.getUsers();
-        console.log(users);
         $(".user > .icon-grab").attr("style", "margin-right: 30px;");
         $(".user > .name").attr("style", "left: 68px");
         $(".user > .icon-woot, .leveldisplay").remove();
         for (i = 0; i < users.length; i++) {
-            console.log(users[i].username + ", " + users[i].vote);
             $(".user > .name:textEquals('" + users[i].username + "')").parent().append(["<i class='icon icon-woot' style='background-position: -174px -280px'></i>", "", "<i class='icon icon-woot'></i>"][users[i].vote + 1] + "<div class='leveldisplay' style='left:30px; height: 30px; width: 46px'><span class='name' style='top: 7px; margin-left: auto; margin-right: auto; color: #eee; font-size: 10px'>lvl" + users[i].level + "</span></div>");
         }
     }
