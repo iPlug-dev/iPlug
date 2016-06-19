112d38b170c2e7cc20d27d22e751dde51ec18b33
Michał <mrcookiemichal@gmail.com>
2016-06-19 16:48:08 +0200

    New tests

--- deploy.js
+++ deploy.js
@@ -9,8 +9,17 @@ function checkVariable(variable) {
 	}
 }
 
+checkVariable("EXTENSION_ID")
 checkVariable("CLIENT_SECRET");
 checkVariable("CLIENT_ID");
+
+var chromeWebStore = new CWS(process.env["CLIENT_ID"], process.env["CLIENT_SECRET"]);
+
+if (typeof process.env["TOKEN"] != "string") {
+	console.log(chromeWebStore.getCodeUrl());
+	console.log("Please visit \n%s\nthen set TOKEN variable on https://gitlab.com/iPlug/iplug/variables", "https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=114021718525-26bjbrg3ju9qv4co3uqa1glm9qlqpbn6.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob");
+	console.log("Don't forget to clear logs! (TOKEN = SENSITIVE DATA)");
+}
 
 var filepath = path.resolve(__dirname, "package.zip");
 var dirpath = path.resolve(__dirname, "Chrome");
