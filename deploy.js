50b9869183b898fab37a59d9fb8927f5a8b34255
Michał <mrcookiemichal@gmail.com>
2016-06-19 17:20:19 +0200

    Almost finished

--- deploy.js
+++ deploy.js
@@ -1,10 +1,13 @@
+var fs = require("fs");
 var path = require("path");
 var CWS = require("chrome-webstore-manager");
 var zipFolder = require("zip-folder");
 
+var variablesLink = "https://gitlab.com/iPlug/iplug/variables";
+
 function checkVariable(variable) {
 	if (typeof process.env[variable] != "string") {
-		console.log("Visit https://gitlab.com/iPlug/iplug/variables and add missing %s variable", variable);
+		console.log("Please visit:\n\n%s\n\nthen add missing %s variable", variablesLink, variable);
 		process.exit(1);
 	}
 }
@@ -16,10 +19,19 @@ checkVariable("CLIENT_ID");
 var chromeWebStore = new CWS(process.env["CLIENT_ID"], process.env["CLIENT_SECRET"]);
 
 if (typeof process.env["TOKEN"] != "string") {
-	console.log(chromeWebStore.getCodeUrl());
-	console.log("Please visit \n%s\nthen set TOKEN variable on https://gitlab.com/iPlug/iplug/variables", "https://accounts.google.com/o/oauth2/auth?response_type=code&scope=https://www.googleapis.com/auth/chromewebstore&client_id=114021718525-26bjbrg3ju9qv4co3uqa1glm9qlqpbn6.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob");
-	console.log("Don't forget to clear logs! (TOKEN = SENSITIVE DATA)");
+	if (typeof process.env["CODE"] != "string") {
+		console.log("Please visit:\n\n%s\n\nthen set CODE variable on https://gitlab.com/iPlug/iplug/variables", chromeWebStore.getCodeUrl());
+		process.exit(1);
+	} else {
+		var token = chromeWebStore.getAccessToken(process.env["CODE"]);		
+		console.log("Please visit:\n\n%s\n\nthen add TOKEN variable = %s", variablesLink, token);
+		console.log("One time warning:");
+		console.log("AFTER YOU COPY TOKEN ERASE LOGS!");
+		process.exit(1);
+	}
 }
+
+console.log("All variables set :)");
 
 var filepath = path.resolve(__dirname, "package.zip");
 var dirpath = path.resolve(__dirname, "Chrome");
@@ -32,4 +44,9 @@ zipFolder(dirpath, filepath, function(err) {
 		process.exit(1);
 	}
 	console.log("Zipped!");
+	console.log("Updating extension %s", process.env["EXTENSION_ID"]);
+	var zip = fs.readFileSync(filepath);
+	chromeWebStore.updateItem(process.env["TOKEN"], zip, function(data) {
+		console.log(data);
+	});
 });
