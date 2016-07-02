15bd1127e3fd80c1980df03c708406b2ac076af5
Michał <mrcookiemichal@gmail.com>
2016-07-02 18:40:41 +0200

    Fixed settings module load, save, clear are invisible

--- Chrome/javascripts/iplug/settings.js
+++ Chrome/javascripts/iplug/settings.js
@@ -20,96 +20,161 @@ define(["iplug/class", "jquery"], function(Class, $) {
             curatebuttons: true,
             topwoot: false,
             topgrab: false,
-            topmeh:  false,
+            topmeh: false,
             topskip: false,
-            topdl:   false,
+            topdl: false,
             roomname: true,
             audience: true,
-            dj:       true,
+            dj: true,
             joinwaitlist: true,
             playbackcontrols: true,
-        }
+        },
         visualizations: {
-            enabled: false,
-            style: 1, // 1 or 2
+            enabled: true,
+            style: 1, // 1 or 2 (coming 3)
             style1: {
                 bars: 150,
                 colors: [{
                     pos: 0.00,
                     color: {
-                        r: 255, g: 0,   b: 0
+                        r: 255,
+                        g: 0,
+                        b: 0
                     }
                 }, {
                     pos: 0.25,
                     color: {
-                        r: 255, g: 255, b: 0
+                        r: 255,
+                        g: 255,
+                        b: 0
                     }
                 }, {
                     pos: 0.50,
                     color: {
-                        r: 0,   g: 255, b: 0
+                        r: 0,
+                        g: 255,
+                        b: 0
                     }
                 }, {
                     pos: 0.75,
                     color: {
-                        r: 0,   g: 255, b: 255
+                        r: 0,
+                        g: 255,
+                        b: 255
                     }
                 }, {
                     pos: 1.00,
                     color: {
-                        r: 0,   g: 0,   b: 255
+                        r: 0,
+                        g: 0,
+                        b: 255
                     }
                 }]
             },
             style2: {
                 colors: [{
-                    r: 105, g: 210, b: 231
+                    r: 105,
+                    g: 210,
+                    b: 231
                 }, {
-                    r: 27,  g: 103, b: 107
+                    r: 27,
+                    g: 103,
+                    b: 107
                 }, {
-                    r: 190, g: 242, b: 2
+                    r: 190,
+                    g: 242,
+                    b: 2
                 }, {
-                    r: 235, g: 229, b: 77
+                    r: 235,
+                    g: 229,
+                    b: 77
                 }, {
-                    r: 0,   g: 205, b: 172
+                    r: 0,
+                    g: 205,
+                    b: 172
                 }, {
-                    r: 22,  g: 147, b: 165
+                    r: 22,
+                    g: 147,
+                    b: 165
                 }, {
-                    r: 249, g: 212, b: 35
+                    r: 249,
+                    g: 212,
+                    b: 35
                 }, {
-                    r: 255, g: 78,  b: 80
+                    r: 255,
+                    g: 78,
+                    b: 80
                 }, {
-                    r: 231, g: 32,  b: 78
+                    r: 231,
+                    g: 32,
+                    b: 78
                 }, {
-                    r: 12,  g: 202, b: 186
+                    r: 12,
+                    g: 202,
+                    b: 186
                 }, {
-                    r: 255, g: 0,   b: 111
+                    r: 255,
+                    g: 0,
+                    b: 111
                 }]
             }
         }
     };
-    var n = Class.extend({
-        localStorageName: "iplug|settings",
-        init: function() {
-            this.load();
-        },
-        clear: function(){
-            localStorage[this.localStorageName] = JSON.stringify(defaults);
-            return this.load();
-        },
-        load: function() {
-            try {
-                var data = JSON.parse(localStorage[this.localStorageName]);
-            } catch (e) {
-                console.warn("Settings parser", e);
+    var settings = {};
+    var localStorageName = "iplug|settings";
+    var n = new Proxy(settings, {
+        get: function(obj, prop) {
+            switch (prop) {
+                case "load":
+                    return function() {
+                        try {
+                            var data = JSON.parse(localStorage[localStorageName]);
+                            if (!data || typeof settings !== "object") {
+                                throw new Error("Not object");
+                            }
+                            for (prop in settings) {
+                                delete settings[prop];
+                            }
+                            for (prop in data) {
+                                settings[prop] = data[prop];
+                            }
+                        } catch (e) {
+                            console.warn("Settings parser", e);
+                            this.clear();
+                        }
+                        return this;
+                    }
+                case "save":
+                    return function() {
+                        localStorage[localStorageName] = JSON.stringify(settings);
+                        return this;
+                    }
+                case "clear":
+                    return function() {
+                        var data = defaults;
+                        for (prop in settings) {
+                            delete settings[prop];
+                        }
+                        for (prop in data) {
+                            settings[prop] = data[prop];
+                        }
+                        return this;
+                    }
+                default:
+                    return obj[prop];
             }
-            $.extend(true, this, defaults, data);
-            return this;
         },
-        save: function() {
-            localStorage[this.localStorageName] = JSON.stringify(this, Object.keys(defaults));
-            return this;
+        set: function(obj, prop, value) {
+            switch (prop) {
+                case "load":
+                case "save":
+                case "clear":
+                    throw new Error("Tried to write non-writable property: " + prop);
+                default:
+                    return obj[prop] = value;
+            }
         }
     });
-    return new n();
+    n.load();
+    return n;
 });
