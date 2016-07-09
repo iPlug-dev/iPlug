bf71a6efb827bdc4611ab40ec089cadda8d1041a
Michał <mrcookiemichal@gmail.com>
2016-07-09 16:41:30 +0200

    Added bg communication handler

--- Chrome/javascripts/iPlugBG.js
+++ Chrome/javascripts/iPlugBG.js
@@ -1,69 +1,69 @@
 chrome.runtime.onUpdateAvailable.addListener(function(details) {
-  console.log("updating to version " + details.version);
-  broadcast("updateChannel", 2);
+    console.log("updating to version " + details.version);
 });
-var ports = [];
-var portsID = [];
 
-chrome.tabCapture.capture({
+/*chrome.tabCapture.capture({
 	audio: true,
 	video: false
 }, function(a) {
 	console.log(a);
+});*/
+
+/*chrome.runtime.onConnect.addListener(function(port) {
+    var ID = Math.floor(Math.random() * 1900);
+    ports.push(port);
+    portsID.push(ID);
+    port.onDisconnect.addListener(function() {
+        for (var i = 0; i < portsID.length; i++) {
+            if (portsID[i] == ID) {
+                portsID.splice(i, 1);
+                ports.splice(i, 1);
+            }
+        }
+    });
+
+    if (port.name == "updateChannel") {
+        port.onMessage.addListener(function(msg) {
+            if (msg == "debug") {
+                port.postMessage(port);
+            } else {
+                chrome.runtime.requestUpdateCheck(function(status) {
+                    if (status == "update_available") {
+                        broadcast(port.name, 1);
+                        console.log("Update pending...");
+                    } else if (status == "no_update") {
+                        broadcast(port.name, 0);
+                        console.log("No update found");
+                    } else if (status == "throttled") {
+                        broadcast(port.name, -1);
+                        console.log("Slow down!");
+                    }
+                });
+            }
+        });
+    }
+});*/
+
+chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
+    console.log("Sender ", sender);
+    console.log("Request", request);
+    setTimeout(function() {
+        sendResponse({
+            "this is callback": "huehe",
+            "json": "object"
+        });
+    }, 3000);
+    return true; //async support for sendResponse
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-  var ID = Math.floor(Math.random() * 1900);
-  ports.push(port);
-  portsID.push(ID);
-  port.onDisconnect.addListener(function() {
-    for (var i = 0;i < portsID.length;i++) {
-      if (portsID[i] == ID) {
-        portsID.splice(i, 1);
-        ports.splice(i, 1);
-      }
-    }
-  });
-  
-  if (port.name == "updateChannel") {
-    port.onMessage.addListener(function(msg) {
-      if (msg == "debug") {
-        port.postMessage(port);
-      } else {
-        chrome.runtime.requestUpdateCheck(function(status) {
-          if (status == "update_available") {
-            broadcast(port.name, 1);
-            console.log("Update pending...");
-          } else if (status == "no_update") {
-            broadcast(port.name, 0);
-            console.log("No update found");
-          } else if (status == "throttled") {
-           broadcast(port.name, -1);
-            console.log("Slow down!");
-          }
-        });
-      }
-    });
-  }
-  
-});
-function broadcast(channelName, data) {
-  for (var i = 0;i < ports.length;i++) {
-    if (ports[i].name == channelName) {
-      ports[i].postMessage(data);
-    }
-  }
-}
-  
 function getRealImage(url, callback) {
-  var xhr = new XMLHttpRequest;
-  xhr.open('GET', url);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4)
-    {
-      var match = xhr.response.match(/[^'"]*(?:image\.prnt|i\.imgur|i\.gyazo\.com\/(?!thumb)|cloudfront.net\\\/images\\\/(?!default))[^'"]*/);
-      callback(match && match[0].replace(/\\\//g, "/") || url);
-    }
-  };
-  xhr.send();
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var match = xhr.response.match(/[^'"]*(?:image\.prnt|i\.imgur|i\.gyazo\.com\/(?!thumb)|cloudfront.net\\\/images\\\/(?!default))[^'"]*/);
+            callback(match && match[0].replace(/\\\//g, "/") || url);
+        }
+    };
+    xhr.send();
 }
