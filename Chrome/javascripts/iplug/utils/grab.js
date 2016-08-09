5fe7c961c1b6a5fc399f8b299c3bb2898492b5e8
Michał <mrcookiemichal@gmail.com>
2016-08-09 18:44:17 +0200

    Fixed grab module & small optimizations for playback

--- Chrome/javascripts/iplug/utils/grab.js
+++ Chrome/javascripts/iplug/utils/grab.js
@@ -1,16 +1,21 @@
-define(["iplug/class", "iplug/plug-modules"], function (Class, Modules) {
+define(["iplug/class", "iplug/plug-modules"], function(Class, Modules) {
     var a = require(Modules["a"]);
     var b = require(Modules["b"]);
+    var s = require(Modules["s"]);
     var q = require(Modules["q"]);
     var n = Class.extend({
-        getPlaylists: function(){
+        getPlaylists: function() {
             return b.toJSON();
         },
         getActivePlaylist: function() {
-            return b.findWhere({active: true}).toJSON();
+            return b.findWhere({
+                active: true
+            }).toJSON();
         },
         getVisiblePlaylist: function() {
-            return b.findWhere({visible: true}).toJSON();
+            return b.findWhere({
+                visible: true
+            }).toJSON();
         },
         grab: function(playlistid) {
             if (!playlistid) {
